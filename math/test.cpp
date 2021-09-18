cf2715ef2b0739f20002dc564010cec504906267
soykot2910 <ashrafulislam2910@gmail.com>
2021-09-18 18:49:21 +0600

    hackerrank prb add

--- math/test.cpp
+++ math/test.cpp
@@ -1,13 +1,37 @@
-
-
 #include <bits/stdc++.h>
+
+void fileRun() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif 
+}
+
 using namespace std;
 
-int main(){
-    int n,m;
-    cin>>n>>m;
-    int r;
-    r=(n-1)*m+(m-1);
-    cout<<r<<endl;
-    return 0;
+int main() {
+  fileRun();
+  int n, q;
+  cin >> n;
+
+  string str[n];
+
+  for (int i = 0;i < n;i++) {
+    cin >> str[i];
+  }
+
+  cin >> q;
+  string checkStr[q];
+  for (int i = 0;i < q;i++) {
+    cin >> checkStr[i];
+  }
+
+  for (int i = 0;i < q;i++) {
+    int flag = 0;
+    for (int j = 0;j < n;j++) {
+      if (checkStr[i] == str[j]) flag++;
+    }
+    cout << flag << endl;
+  }
+
 }
